2bb2b7e6f08911be4236126b8f3dc027ccb404c2
SpaceArk7 <spacearkchief@gmail.com>
2021-08-12 19:54:11 +0700

    Theme Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pickeat_app/common/style.dart';
+import 'package:pickeat_app/ui/homeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,18 +13,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PickEat',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+       primaryColor: primaryBrandColor,
+       accentColor: secondaryBrandColor,
+       dividerColor: accentBrandColor,
+       scaffoldBackgroundColor: backgoundColor,
+       visualDensity: VisualDensity.adaptivePlatformDensity,
+       textTheme: brandTextTheme,
+       appBarTheme: AppBarTheme(
+         textTheme: brandTextTheme.apply(bodyColor: Colors.black),
+         elevation: 0
+       ),
+       bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: secondaryBrandColor,
+          unselectedItemColor: Colors.grey,
+        ),
       ),
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }
@@ -37,7 +43,7 @@ class SplashScreen extends StatelessWidget {
         decoration: BoxDecoration (
          image: DecorationImage(image: 
          AssetImage('assets/images/s1.jpg',),
-         fit: BoxFit.fitWidth)
+         fit: BoxFit.cover)
         ),
 
         child: Padding(
---
+++ lib/ui/homeScreen.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pickeat_app/widgets/platformWidget.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/home_screen';
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _bottomNavIndex = 0;
+
+  List<BottomNavigationBarItem> _bottomNavBarItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Platform.isIOS ? CupertinoIcons.news : Icons.public),
+        label: 'Home'),
+    BottomNavigationBarItem(
+        icon: Icon(Platform.isIOS ? CupertinoIcons.heart : Icons.favorite),
+        label: 'Favorite'),
+    BottomNavigationBarItem(
+        icon: Icon(Platform.isIOS ? CupertinoIcons.person : Icons.person),
+        label: 'Profile'),
+  ];
+
+  void _onBottomNavTapped(int index) {
+    setState(() {
+      _bottomNavIndex = index;
+    });
+  }
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _bottomNavIndex,
+        items: _bottomNavBarItems,
+        onTap: _onBottomNavTapped,
+      ),
+    );
+  }
+
+  Widget _buildIOS(BuildContext context) {
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: _bottomNavBarItems,
+        ),
+        tabBuilder: (context, index) {
+          return Container();
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(androidBuilder: _buildAndroid, iosBuilder: _buildIOS);
+  }
+}
